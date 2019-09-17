1d84f681bcbe3bb7f10f890439759057880a8b5a
emrejz <emre.kayacan@msn.com>
2019-09-17 21:30:07 +0300

    redis problems1

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -11,10 +11,10 @@ module.exports = server => {
   io.use(socketAuth);
   io.adapter(
     redisAdapter({
-      url: process.env.REDISCLOUD_GRAY_URL
-      // host: process.env.REDIS_CLOUD_HOST,
-      // port: process.env.REDIS_CLOUD_PORT,
-      // auth_pass: process.env.REDIS_CLOUD_PASS
+      //  url: process.env.REDISCLOUD_GRAY_URL
+      host: process.env.REDISCLOUD_GRAY_HOST,
+      port: process.env.REDISCLOUD_GRAY_PORT,
+      auth_pass: process.env.REDISCLOUD_GRAY_PASS
     })
   );
 

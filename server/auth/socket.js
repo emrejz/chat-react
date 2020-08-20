62459f0f573fb07b0d2c2a671d20ef7466f2d5ac
emre kayacan <emre.kayacan@msn.com>
2020-08-20 22:29:08 +0300

    deleted passport prop

--- server/auth/socket.js
+++ server/auth/socket.js
@@ -1,16 +1,14 @@
 const RedisStore = require("../redis/redisStore"); // find a working session store (have a look at the readme)
 const passportSocketIo = require("passport.socketio");
 const cookieParser = require("cookie-parser");
-const passport = require("passport");
 
 module.exports = passportSocketIo.authorize({
-  passport,
   cookieParser, // the same middleware you registrer in express
   key: "connect.sid", // the name of the cookie where express/connect stores its session_id
   secret: process.env.SECRET_KEY, // the session_secret to parse the cookie
   store: RedisStore, // we NEED to use a sessionstore. no memorystore please
   success: onAuthorizeSuccess, // *optional* callback on success - read more below
-  fail: onAuthorizeFail
+  fail: onAuthorizeFail,
 });
 function onAuthorizeSuccess(data, accept) {
   console.log("successful connection to socket.io");
